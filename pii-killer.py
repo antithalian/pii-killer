3bb4a94076b1ac370e8069d9dd534d3bc926a8c9
John Sullivan <jpsullivan2019@gmail.com>
2020-06-06 12:04:55 -0700

    Create code and configuration skeleton

---
+++ pii-killer.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+# pii-killer.py
+# see README for functionality explanation
+
+# imports
+import praw
+import logging
+import json
+
+# load configuration
+
+# connect to reddit via praw
