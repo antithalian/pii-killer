5060b64aca2c71541ed38b5ab272c02397b19711
John Sullivan <jpsullivan2019@gmail.com>
2020-07-05 03:38:32 -0700

    secrets.json.example --> example-secrets.json

--- pii-killer.py
+++ pii-killer.py
@@ -9,5 +9,10 @@ import json
 import os
 
 # load configuration
+with open("secrets.json", "r") as secrets:
+    secs = json.load(secrets)
 
 # connect to reddit via praw
+reddit = praw.Reddit(client_id=secs["client_id"], client_secret=secs["client_secret"], 
+                    password=secs["password"], username=secs["username"], 
+                    user_agent="PII killing script")
